e7841eb2fb1c62516e53e29c1395dd1d2edc7c10
Harshj1 <harshrjain98@gmail.com>
2017-01-28 19:00:57 +0530

    part1

--- app/src/main/java/com/scorpion/Main2Activity.java
+++ app/src/main/java/com/scorpion/Main2Activity.java
@@ -2,6 +2,8 @@ package com.scorpion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        addItemsOnSpinner2();
+    }
+    public void addItemsOnSpinner2(){
+        Spinner spinner= (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter <CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.language,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter1);
     }
 
     
