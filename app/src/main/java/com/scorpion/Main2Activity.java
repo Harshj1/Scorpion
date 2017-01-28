bc61c8a7a1880d1795848898a968ee825b148e91
adityagaikwad <adityagaikwad009@gmail.com>
2017-01-28 21:16:57 +0530

    Rutvij

--- app/src/main/java/com/scorpion/Main2Activity.java
+++ app/src/main/java/com/scorpion/Main2Activity.java
@@ -9,19 +9,49 @@ import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity {
 
+    String array_name = "states_e";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        addItemsOnSpinner1();
         addItemsOnSpinner2();
+
     }
-    public void addItemsOnSpinner2(){
+    public void addItemsOnSpinner1(){
         Spinner spinner= (Spinner) findViewById(R.id.spinner);
         ArrayAdapter <CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.language,android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(adapter1);
+        check_language();
     }
 
+    public void addItemsOnSpinner2(){
+        Spinner spinner= (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter <CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.states_e,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter1);
+    }
+
+    public void check_language()
+    {
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        String text = spinner.getSelectedItem().toString();
+        if (text == "English")
+        {
+
+        }
+        else if (text == "हिंदी")
+        {
+            addItemsOnSpinner3();
+        }
+    }
+    public void addItemsOnSpinner3(){
+        Spinner spinner= (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter <CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.states_hindi,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter1);
+    }
     void register(View view){
         Intent intent=new Intent(this,MainActivity.class);
         startActivity(intent);
