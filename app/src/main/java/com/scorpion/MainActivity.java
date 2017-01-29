1584996021351597fa6d93ed79fcfc048e8a37db
adityagaikwad <adityagaikwad009@gmail.com>
2017-01-29 07:12:17 +0530

    Recycler

--- app/src/main/java/com/scorpion/MainActivity.java
+++ app/src/main/java/com/scorpion/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
